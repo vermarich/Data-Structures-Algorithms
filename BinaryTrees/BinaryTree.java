6349ab750604baacaf3d223686117b487f758052
richa <richa@allgovision.com>
2019-09-06 18:27:53 +0530

    Level order traversal in Binary tree

--- BinaryTrees/BinaryTree.java
+++ BinaryTrees/BinaryTree.java
@@ -43,6 +43,30 @@ class BinaryTree {
         return max;
     }
 
+    public void levelOrderTraversal(Node rt) {
+        if(rt == null) return;
+
+        Queue<Node> q = new LinkedList<>();  
+        q.add(rt);
+        q.add(null);
+        while(!q.isEmpty()){
+            Node temp = q.remove();
+            if(temp == null){
+                if(q.size() == 0) return;
+                System.out.println("");
+                q.add(null);
+                continue;
+            }
+            System.out.print(temp.data + " ");
+            if(temp.left != null){
+                q.add(temp.left);
+            }
+            if(temp.right != null){
+                q.add(temp.right);
+            }
+        }    
+    }
+
     public static void main(String args[]) {
         BinaryTree bt = new BinaryTree(10);
         bt.root.left = new Node(5);
@@ -54,6 +78,7 @@ class BinaryTree {
         System.out.println("Left node ==> "+ bt.root.left.data);
         System.out.println("Right node ==> "+ bt.root.right.data);
         System.out.println("Maximum in Binary Tree => " + bt.findMaxBtRecursive(bt.root));
+        bt.levelOrderTraversal(bt.root);
     }
 
 }
