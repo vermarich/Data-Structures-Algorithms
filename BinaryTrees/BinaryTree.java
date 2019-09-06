a5ade61ab928fb3dbe8e6585f9006a56747d4be5
richa <richa@allgovision.com>
2019-09-06 17:53:38 +0530

    maximum in a binary tree recursive

--- BinaryTrees/BinaryTree.java
+++ BinaryTrees/BinaryTree.java
@@ -24,16 +24,36 @@ class BinaryTree {
         root = null;
     }
 
+    public int findMaxBtRecursive(Node rt) {
+        if(root.left == null && root.right == null) {
+            return root.data;
+        }
+
+        int max = rt.data;
+        int leftMax = rt.left == null ? rt.data : findMaxBtRecursive(rt.left);
+        int rightMax = rt.right == null ? rt.data : findMaxBtRecursive(rt.right);
+
+        if(max<leftMax){
+            max = leftMax;
+        }
+        if(max<rightMax){
+            max = rightMax;
+        }
+
+        return max;
+    }
+
     public static void main(String args[]) {
         BinaryTree bt = new BinaryTree(10);
         bt.root.left = new Node(5);
         bt.root.right = new Node(12);
-
-        // bt.setLeft(5);
-        // bt.setRight(12);
+        bt.root.left.left = new Node(-13);
+        bt.root.left.right = new Node(50);
+        bt.root.right.right = new Node(100);
         System.out.println("Root node ==> "+ bt.root.data);
         System.out.println("Left node ==> "+ bt.root.left.data);
         System.out.println("Right node ==> "+ bt.root.right.data);
+        System.out.println("Maximum in Binary Tree => " + bt.findMaxBtRecursive(bt.root));
     }
 
 }
