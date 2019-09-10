0d35499b42c725ada1ad29eff8cee793c11dabd7
richa <richa@allgovision.com>
2019-09-10 12:24:53 +0530

    Find maximum depth of Binary Tree

--- BinaryTrees/BinaryTree.java
+++ BinaryTrees/BinaryTree.java
@@ -15,6 +15,7 @@ class Node {
 
 class BinaryTree {
     Node root;
+    static int maxDepth;
 
     BinaryTree(int val) {
         root = new Node(val);
@@ -94,11 +95,27 @@ class BinaryTree {
         }
     }
 
-    // public void deleteBinaryTree(Node rt) {
-    //     if(rt.left == null && rt.right == null) return null;
-    //     rt.left = 
+    public void deleteBinaryTree(Node rt) {
+        
+        if(rt == null) return;
+            
+        deleteBinaryTree(rt.left);
+        deleteBinaryTree(rt.right);
 
-    // }
+        rt = null;
+
+    }
+
+    public void deepestNodeInBt(Node rt, int currDepth) {
+        if(rt == null) return;
+
+        currDepth ++;
+        if(currDepth > maxDepth)
+            maxDepth = currDepth;
+
+        deepestNodeInBt(rt.left, currDepth);
+        deepestNodeInBt(rt.right, currDepth);
+    }
 
     public static void main(String args[]) {
         BinaryTree bt = new BinaryTree(10);
@@ -107,6 +124,7 @@ class BinaryTree {
         bt.root.left.left = new Node(-13);
         bt.root.left.right = new Node(50);
         bt.root.right.right = new Node(100);
+        bt.root.right.right.left = new Node(200);
         System.out.println("Root node ==> "+ bt.root.data);
         System.out.println("Left node ==> "+ bt.root.left.data);
         System.out.println("Right node ==> "+ bt.root.right.data);
@@ -116,6 +134,9 @@ class BinaryTree {
         System.out.println("Is the value "+ 12 + " present in this Binary Tree ? "+ bt.searchElementInBtRecursive(bt.root, 12));
         System.out.println("Binary tree elements printed in reverse order "); 
         bt.printElementsInReverseLOT(bt.root);
+        System.out.println(" ");
+        bt.deepestNodeInBt(bt.root,0);
+        System.out.println("Maximum depth of Binary Tree "+ maxDepth);
     }
 
 }
